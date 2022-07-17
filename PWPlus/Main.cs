0ac7a35684409ee3e274242e988cb241f7762715
Jedlord <71676806+Jedlord@users.noreply.github.com>
2022-07-17 17:57:58 -0500

    Add files via upload

--- PWPlus/Main.cs
+++ PWPlus/Main.cs
@@ -1,1 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MelonLoader;
+using UnityEngine;
 
+
+namespace PWPlus
+{
+    public class PWPlus : MelonMod
+    {
+        public static MelonLogger.Instance Logger;
+        public static Tab tab = Tab.None;
+        public static World world;
+        private static int totalgems;
+        private static string worldToWarp = "";
+        private static bool showMenu = true;
+        private static Rect windowSize = new Rect(Screen.width / 2 - 200, Screen.height / 2 - 200, 250, 250);
+
+        // on startup
+        public override void OnApplicationStart()
+        {
+            LoggerInstance.Msg(ConsoleColor.Cyan, "Starting PW Plus...");
+            Logger = LoggerInstance;
+            
+        }
+        
+        // on quit
+        public override void OnApplicationQuit()
+        {
+            LoggerInstance.Msg(ConsoleColor.Cyan, "Quitting PW Plus...");
+        }
+        
+        // tab enum
+        public enum Tab
+        {
+            None,
+            GemCalculator,
+            WorldWarper,
+            GemSeller
+        }
+
+
+        private void MyWindow(int id)
+        {
+            // buttons to switch tabs
+            if (GUI.Button(new Rect(0f, 16.5f, 83.333f, 33f), "Gem Calc"))
+            {
+                tab = Tab.GemCalculator;
+            }
+            if (GUI.Button(new Rect(83.333f, 16.5f, 83.333f, 33f), "World Warp"))
+            {
+                tab = Tab.WorldWarper;
+            }
+            if (GUI.Button(new Rect(166.600f, 16.5f, 83.333f, 33f), "Misc"))
+            {
+                tab = Tab.GemSeller;
+            }
+
+            // check value of tab to see what tab to render
+            switch (tab)
+            {
+                case Tab.None:
+                    // No tab open
+
+                    break;
+
+                case Tab.GemCalculator:
+                    // Gem Calculator Tab
+                    GUI.Label(new Rect(5f, 50f, 100f, 20f), "Gem Calculator");
+
+                    if (GUI.Button(new Rect(5f, 75f, 115f, 33f), "Calculate Gems"))
+                    {
+                        if (world != null)
+                        {
+                            totalgems = 0;
+
+                            foreach (CollectableData collectable in world.collectables)
+                            {
+                                totalgems += (Tools.GemCalculator.GetGemAmount(collectable.blockType) * collectable.amount);
+                            }
+                        }
+                        else
+                        {
+                            totalgems = 0;
+                        }
+                    }
+                    GUI.Label(new Rect(5f, 108f, 100f, 20f), $"Gems: {totalgems}");
+
+                    break;
+
+                case Tab.WorldWarper:
+                    // Warp World Tab
+                    GUI.Label(new Rect(5f, 50f, 100f, 20f), "Warp To World");
+                    worldToWarp = GUI.TextField(new Rect(125f, 75f, 85f, 33f), worldToWarp);
+                    if (GUI.Button(new Rect(5f, 75f, 115f, 33f), "Warp To World"))
+                    {
+                        SceneLoader.CheckIfWeCanGoFromWorldToWorld(worldToWarp, "", null, false, null);
+                    }
+
+                    break;
+
+                case Tab.GemSeller:
+                    // Misc Tab
+                    GUI.Label(new Rect(5f, 50f, 100f, 20f), "Gem Seller");
+                    if (GUI.Button(new Rect(5, 75f, 115f, 33f), "Test"))
+                    {
+                        Logger.Msg(ConsoleColor.Yellow, "Test");
+                    }
+                    break;
+
+                default:
+                    // If it is none of these
+                    break;
+            }
+
+            GUI.DragWindow();
+        }
+
+        public override void OnGUI()
+        {
+            // Watermark rendering
+            GUIStyle style = new GUIStyle();
+            style.fontStyle = FontStyle.Bold;
+            style.fontSize = 60;
+            style.normal.textColor = Color.white;
+            GUI.Label(new Rect(Screen.width - 280, 40, 480, 180), "PW Plus", style);
+
+
+            // Render the menu if showMenu is true
+            if (showMenu)
+            {
+                windowSize = GUI.Window(0, windowSize, (GUI.WindowFunction)MyWindow, "PW Plus Window");
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                showMenu = !showMenu;
+            }
+        }
+
+    }
+}
---
+++ PWPlus/Patches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace PWPlus
+{
+    [HarmonyPatch(typeof(World), "GenerateLayersFromBSON")]
+    public static class World_GenerateLayersFromBSON
+    {
+        public static void Prefix(World __instance)
+        {
+            PWPlus.world = __instance;
+        }
+    }
+}
