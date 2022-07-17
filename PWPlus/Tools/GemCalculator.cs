d89526936a9827089d5c8fe9125a6010af971a1d
Jedlord <71676806+Jedlord@users.noreply.github.com>
2022-07-17 17:58:47 -0500

    Add files via upload

---
+++ PWPlus/Tools/GemCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace PWPlus.Tools
+{
+    public static class GemCalculator
+    {
+        public static int GetGemAmount(World.BlockType blockType)
+        {
+            if(ConfigData.IsFish(blockType))
+            {
+                return ConfigData.GetFishRecycleValueForFishRecycler(blockType);
+            }
+            if(ConfigData.IsBlockMiningGemstone(blockType))
+            {
+                return ConfigData.GetGemstoneRecycleValueForMiningGemstoneRecycler(blockType);
+            }
+            if(ConfigData.IsConsumableTreasurePouch(blockType))
+            {
+                return ConfigData.GetTreasurePouchRewardAmount(blockType);
+            }
+
+            return 0;
+        }
+    }
+}
