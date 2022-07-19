c48bb58ab82e877ed20e98324556ce099ddd5791
Jedlord <71676806+Jedlord@users.noreply.github.com>
2022-07-19 18:36:04 -0500

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace PWPlus
+{
+    [HarmonyPatch(typeof(World), "GenerateLayersFromBSON")]
+    public static class World_GenerateLayersFromBSON
+    {
+        public static void Prefix(World __instance)
+        {
+            PWPlus.world = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerData), "AddListenerForVIPAmountChanged")]
+    public static class PlayerData_AddListenerForVIPAmountChanged
+    {
+        public static void Prefix(PlayerData __instance)
+        {
+            PWPlus.playerData = __instance;
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), "Awake")]
+    public static class Player_Awake
+    {
+        public static void Prefix(Player __instance)
+        {
+            if (__instance.myPlayerData == PWPlus.playerData)
+            {
+                PWPlus.player = __instance;
+            }
+        }
+    }
+}
