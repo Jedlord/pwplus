65a614dd4cf0ed5f9905fed9a3cc97c2f116db84
Jedlord <71676806+Jedlord@users.noreply.github.com>
2023-04-15 09:53:26 -0500

    Removed Player and PlayerData instance patches

--- PWPlus/Patches.cs
+++ PWPlus/Patches.cs
@@ -12,25 +12,4 @@ namespace PWPlus
             PWPlus.world = __instance;
         }
     }
-
-    [HarmonyPatch(typeof(PlayerData), "AddListenerForVIPAmountChanged")]
-    public static class PlayerData_AddListenerForVIPAmountChanged
-    {
-        public static void Prefix(PlayerData __instance)
-        {
-            PWPlus.playerData = __instance;
-        }
-    }
-
-    [HarmonyPatch(typeof(Player), "Awake")]
-    public static class Player_Awake
-    {
-        public static void Prefix(Player __instance)
-        {
-            if (__instance.myPlayerData == PWPlus.playerData)
-            {
-                PWPlus.player = __instance;
-            }
-        }
-    }
 }
